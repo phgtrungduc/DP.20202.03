0b0b4938c39f7c07a21224157956c3a38fb595b2
xuanduonghoa <duonghx.vnist@gmail.com>
2021-03-12 21:11:15 +0700

    cohension

--- src/main/java/controller/SessionInformation.java
+++ src/main/java/controller/SessionInformation.java
@@ -8,16 +8,6 @@ import java.time.LocalDateTime;
 /**
  * @author
  */
-
-//content couling, common coupling
-    /**
-     * Content couling, Common coupling
-    * các thuộc tính đều ở dạng public, tuy nhiên không cung cấp
-    * các phương thức get và set để thay đổi chúng nên các module
-    * sử dụng đều phải truy cập trực tiếp vào thuộc tính để thay đổi
-     * Gây ra cả content và common ở một số module
-     * ptduc
-    * */
 public class SessionInformation {
 
     public static User mainUser;
